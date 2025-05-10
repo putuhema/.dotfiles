b2a442a777e3b8b2f544c73ae3d68b9988dd7b55
putuhema <putuhendramahendra@gmail.com>
2025-05-10 17:39:23 +0800

    fix(lsp): idk what happen

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,141 +1,185 @@
 return {
-  -- tools
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "tailwindcss-language-server",
-        "typescript-language-server",
-        "css-lsp",
+        "stylua", -- Lua formatter
+        "selene", -- Lua linter
+        "luacheck", -- Lua linter
+        "shellcheck", -- Shell script linter
+        "shfmt", -- Shell script formatter
+        "prettierd", -- JS/TS/JSX/TSX formatter
+        "eslint_d", -- JS/TS linter
+        "php-cs-fixer", -- PHP formatter
+        "phpstan", -- PHP static analysis
+        "css-lsp", -- CSS language server
+        "typescript-language-server", -- TypeScript/JavaScript (React, Next.js, Astro, Vue, Svelte
       })
     end,
   },
 
-  -- lsp servers
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "cssls", -- CSS
+          "html", -- HTML
+          "yamlls", -- YAML
+          "lua_ls", -- Lua
+          "eslint", -- JS/TS linting
+          "phpactor", -- PHP (Laravel, general PHP)
+          "astro", -- Astro
+          "svelte", -- Svelte
+          "vuels", -- Vue
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = { enabled = false },
-      ---@type lspconfig.options
-      servers = {
-        cssls = {},
-        tailwindcss = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- TypeScript/JavaScript (React, Next.js, Astro, Vue, Svelte)
+      lspconfig.tsserver.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false -- Let prettier handle formatting
+        end,
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
         },
-        tsserver = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
-          single_file_support = false,
-          settings = {
-            typescript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "literal",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = false,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
             },
-            javascript = {
-              inlayHints = {
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayVariableTypeHints = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayEnumMemberValueHints = true,
-              },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
             },
           },
         },
-        html = {},
-        yamlls = {
-          settings = {
-            yaml = {
-              keyOrdering = false,
+      })
+
+      -- ESLint for linting JavaScript/TypeScript
+      lspconfig.eslint.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+          "astro",
+        },
+      })
+
+      -- PHP (Laravel, general PHP)
+      lspconfig.phpactor.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false -- Let php-cs-fixer handle formatting
+        end,
+        filetypes = { "php" },
+        init_options = {
+          ["language_server_php_cs_fixer.enabled"] = true,
+          ["language_server_phpstan.enabled"] = true,
+        },
+      })
+
+      -- Astro
+      lspconfig.astro.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+      })
+
+      -- Svelte
+      lspconfig.svelte.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+      })
+
+      -- Vue
+      lspconfig.vuels.setup({
+        on_attach = function(client)
+          client.server_capabilities.documentFormattingProvider = false
+        end,
+      })
+
+      -- CSS
+      lspconfig.cssls.setup({
+        settings = {
+          css = {
+            validate = true,
+            lint = {
+              unknownAtRules = "ignore", -- Support modern CSS frameworks
             },
           },
         },
-        lua_ls = {
-          -- enabled = false,
-          single_file_support = true,
-          settings = {
-            Lua = {
-              workspace = {
-                checkThirdParty = false,
-              },
-              completion = {
-                workspaceWord = true,
-                callSnippet = "Both",
-              },
-              misc = {
-                parameters = {
-                  -- "--log-level=trace",
-                },
-              },
-              hint = {
-                enable = true,
-                setType = false,
-                paramType = true,
-                paramName = "Disable",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-              doc = {
-                privateName = { "^_" },
-              },
-              type = {
-                castNumberToInteger = true,
-              },
-              diagnostics = {
-                disable = { "incomplete-signature-doc", "trailing-space" },
-                -- enable = false,
-                groupSeverity = {
-                  strong = "Warning",
-                  strict = "Warning",
-                },
-                groupFileStatus = {
-                  ["ambiguity"] = "Opened",
-                  ["await"] = "Opened",
-                  ["codestyle"] = "None",
-                  ["duplicate"] = "Opened",
-                  ["global"] = "Opened",
-                  ["luadoc"] = "Opened",
-                  ["redefined"] = "Opened",
-                  ["strict"] = "Opened",
-                  ["strong"] = "Opened",
-                  ["type-check"] = "Opened",
-                  ["unbalanced"] = "Opened",
-                  ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
-              },
-              format = {
-                enable = false,
-                defaultConfig = {
-                  indent_style = "space",
-                  indent_size = "2",
-                  continuation_indent_size = "2",
-                },
-              },
+      })
+
+      -- HTML
+      lspconfig.html.setup({})
+
+      -- YAML (useful for Laravel configs, GitHub Actions)
+      lspconfig.yamlls.setup({
+        settings = {
+          yaml = {
+            keyOrdering = false,
+            schemas = {
+              ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+              ["https://json.schemastore.org/composer.json"] = "/composer.json",
             },
           },
         },
-      },
-      setup = {},
-    },
+      })
+
+      -- Lua (for Neovim config)
+      lspconfig.lua_ls.setup({
+        single_file_support = true,
+        settings = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            completion = { workspaceWord = true, callSnippet = "Both" },
+            format = { enable = false }, -- Let stylua handle formatting
+            diagnostics = {
+              globals = { "vim" }, -- Recognize Neovim globals
+            },
+          },
+        },
+      })
+
+      -- Enable inlay hints globally
+      vim.lsp.inlay_hint.enable(true)
+    end,
   },
+
+  -- Custom keymaps for LSP
   {
     "neovim/nvim-lspconfig",
     opts = function()
@@ -144,11 +188,31 @@ return {
         {
           "gd",
           function()
-            -- DO NOT RESUSE WINDOW
             require("telescope.builtin").lsp_definitions({ reuse_win = false })
           end,
           desc = "Goto Definition",
           has = "definition",
+        },
+        {
+          "gr",
+          function()
+            require("telescope.builtin").lsp_references({ reuse_win = false })
+          end,
+          desc = "Goto References",
+          has = "references",
+        },
+        {
+          "<leader>ca",
+          vim.lsp.buf.code_action,
+          desc = "Code Action",
+          mode = { "n", "v" },
+          has = "codeAction",
+        },
+        {
+          "<leader>rn",
+          vim.lsp.buf.rename,
+          desc = "Rename",
+          has = "rename",
         },
       })
     end,
