5e69562cc569dd0bd8b0d4a50ee8bcd48dc367a4
putuhema <putuhendramahendra@gmail.com>
2025-05-10 17:41:41 +0800

    style: change colorscheme

--- lua/plugins/coloscheme.lua
+++ lua/plugins/coloscheme.lua
@@ -1,77 +1,20 @@
--- return {
---   {
---     "ellisonleao/gruvbox.nvim",
---     priority = 1000,
---     config = function()
---       require("gruvbox").setup({
---         transparenT_mode = true,
---         terminal_colors = true,
---       })
---       vim.cmd("colorscheme gruvbox")
---     end,
---   },
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "gruvbox",
---     },
---   },
--- }
-
 return {
   {
-    "vague2k/vague.nvim",
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
     config = function()
-      require("vague").setup({
-        -- optional configuration here
-        -- transparent = true,
-        style = {
-          -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-          boolean = "none",
-          number = "none",
-          float = "none",
-          error = "none",
-          comments = "none",
-          conditionals = "none",
-          functions = "none",
-          headings = "bold",
-          operators = "none",
-          strings = "none",
-          variables = "none",
-
-          -- keywords
-          keywords = "none",
-          keyword_return = "none",
-          keywords_loop = "none",
-          keywords_label = "none",
-          keywords_exception = "none",
-
-          -- builtin
-          builtin_constants = "none",
-          builtin_functions = "none",
-          builtin_types = "none",
-          builtin_variables = "none",
-        },
-        colors = {
-          -- func = "#bc96b0",
-          -- keyword = "#787bab",
-          -- -- string = "#d4bd98",
-          -- string = "#8a739a",
-          -- -- string = "#f2e6ff",
-          -- -- number = "#f2e6ff",
-          -- -- string = "#d8d5b1",
-          -- number = "#8f729e",
-          -- -- type = "#dcaed7",
-        },
+      require("gruvbox").setup({
+        transparenT_mode = true,
+        terminal_colors = true,
       })
+      vim.cmd("colorscheme gruvbox")
     end,
   },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vague",
+      colorscheme = "gruvbox",
     },
   },
 }
